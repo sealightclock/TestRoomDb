4b67d58a37f0a1cd11fabf83f86676e7ec374481
Sealight Clock <chbebram@hotmail.com>
2024-12-10 12:19:43 -0800

    from BasicTextField to OutlinedTextField

--- app/src/main/java/com/example/jonathan/testroomdb/view/UserScreen.kt
+++ app/src/main/java/com/example/jonathan/testroomdb/view/UserScreen.kt
@@ -6,8 +6,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -31,31 +31,37 @@ fun UserScreen(viewModel: UserViewModel) {
         verticalArrangement = Arrangement.Center
     ) {
         Text("Please enter user name:")
-        BasicTextField(
+        OutlinedTextField(
             value = name,
             onValueChange = { name = it },
             modifier = Modifier.fillMaxWidth().padding(8.dp),
             singleLine = true
         )
+
         Text("Please enter user's age:")
-        BasicTextField(
+        OutlinedTextField(
             value = age,
             onValueChange = { age = it },
             modifier = Modifier.fillMaxWidth().padding(8.dp),
             singleLine = true
         )
+
         Button(onClick = {
             viewModel.addUser(name, age.toIntOrNull() ?: 0)
         }) {
             Text("Add User")
         }
+
         Spacer(modifier = Modifier.height(16.dp))
+
         Button(onClick = {
             viewModel.fetchUsers()
         }) {
             Text("Load Users")
         }
+
         Spacer(modifier = Modifier.height(16.dp))
+
         users.forEach {
             Text("user_${it.id}: Name: ${it.name}, Age: ${it.age}")
         }
